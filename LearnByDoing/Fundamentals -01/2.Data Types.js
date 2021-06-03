2a84ba8a8fb98964b4ccab33c86eb8a67377516e
Ajit <67835881+Aj7t@users.noreply.github.com>
2021-06-03 13:47:56 +0530

    Fundamentals
    Learning JavaScript fundamentals concept By Doing

---
+++ LearnByDoing/Fundamentals -01/2.Data Types.js
@@ -0,0 +1,29 @@
+/***************************************************************************
+
+@Program : Data Types
+
+@problem_Statement : 
+1. Declare a variable called 'isIsland' and set its value according to your 
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console 
+
+******************************************************************************/
+
+// Declare variable 
+let isIsland = false;  
+let language;
+let country = 'India';
+let population = 10000;
+
+// typeof() datatype
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+
+//Null datatype
+
+// contributed by aj7t
